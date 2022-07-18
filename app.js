f3a606b07d4961972d994ea744ab33a64aa3c06c
rogerespinoza1 <86305792+rogerespinoza1@users.noreply.github.com>
2022-07-17 21:21:23 -0500

    first commit

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hola mundo de porqueria</h1>');
+  res.end('<h1>Hola mundo de porqueria gaaaaaaaaaaa</h1>');
 });
 
 server.listen(port,() => {
-  console.log(`Server running at port `+port);
+  console.log(`Server running at port`+port);
 });
